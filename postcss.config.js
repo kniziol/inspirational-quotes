feffc26df999b76b90371f014d5e779921156a45
Krzysztof Nizioł <kniziol@gmail.com>
2026-09-20 13:09:45 +0200

    Update all npm packages to the latest versions (#5)
    * Update all npm packages to the latest versions
    * Fix code style of the PostCSS configuration file
    * Fix the "Cannot find package '@eslint/js' imported from eslint.config.js" error when running ESLint

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    tailwindcss({
+      base: path.resolve(__dirname, './src/routes'),
+    }),
+    autoprefixer(),
+  ],
 };
