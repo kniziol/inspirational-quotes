0822e8a7edd862e90326a931e2d6a673f4a749fb
Krzysztof Nizioł <kniziol@gmail.com>
2026-09-21 19:10:10 +0200

    Replace slot with snippet for rendering content
    https://svelte.dev/docs/svelte/legacy-slots

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
   import '../app.scss';
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    children: Snippet;
+  }
+
+  let { children }: Props = $props();
 </script>
 
-<slot />
+{@render children()}
